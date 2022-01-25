d0e071bab217486a3b046987cb4d9ac79a39e926
Casey-Viens <casey.viens@gmail.com>
2022-01-24 21:59:45 -0500

    in class database example

--- Week3/templateExample/app.js
+++ Week3/templateExample/app.js
@@ -6,7 +6,16 @@ const app = express()
 //Set the view engine
 app.engine('hbs', exphbs.engine({
     defaultlayout: 'main',
-    extname: '.hbs'
+    extname: '.hbs',
+    helpers: {
+        getShortComment(comment){
+            if(comment.length < 60){
+                return comment
+            }else{
+                return comment.substring(0, 60) + "..."
+            }
+        }
+    }
 }))
 
 app.set('view engine', 'hbs')
@@ -14,7 +23,7 @@ app.set('view engine', 'hbs')
 //route to render the page
 app.get('/',(req,res)=>{
     res.render('home', {
-        posts:[{
+        post:[{
             author: "Casey Viens",
             image: "https://picsum.photos/500/500",
             comments: ["comment 1", "comment 2", "comment 3"],
@@ -27,7 +36,7 @@ app.get('/',(req,res)=>{
         {
             author:"Jon D",
             image: "https://picsum.photos/500/500?3",
-            comments: ["Yo, this is awesome", "How'd you do that", "this is the best"],
+            comments: ["Yo, this is awesome", "Sint dolor aliquip deserunt ipsum sunt elit et. Est commodo eu dolore adipisicing mollit Lorem. Occaecat ullamco culpa officia excepteur occaecat nostrud amet. Est ex pariatur consectetur laboris. Labore irure velit culpa enim. Incididunt laboris nostrud duis proident magna id nisi irure.", "this is the best"],
         }
     ]
         
